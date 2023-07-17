32dafb2b4bc729fbef28f83d6fd113ab3a7e2d65
Rohitash Singh <rohitashmtr2002@gmail.com>
2023-07-17 15:19:42 +0530

    Added extra attribute in contact form

--- superstack/src/app/app.module.ts
+++ superstack/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { AngularFireModule } from '@angular/fire/compat'; 
-// import { FirestoreModule } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire/compat'; 
+import { FirestoreModule } from '@angular/fire/firestore';
  
 
 import { FormsModule } from '@angular/forms';
@@ -12,15 +12,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms'; 
 import { ContactFormComponent } from './contact-form/contact-form.component';
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA4eTZ-yGEueO8mCcikULXR-IQEiu73K1M",
-//   authDomain: "superstack-85719.firebaseapp.com",
-//   projectId: "superstack-85719",
-//   storageBucket: "superstack-85719.appspot.com",
-//   messagingSenderId: "373608190954",
-//   appId: "1:373608190954:web:f32040073c79abbfe09893",
-//   measurementId: "G-YCEP86N7K7"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyA4eTZ-yGEueO8mCcikULXR-IQEiu73K1M",
+  authDomain: "superstack-85719.firebaseapp.com",
+  projectId: "superstack-85719",
+  storageBucket: "superstack-85719.appspot.com",
+  messagingSenderId: "373608190954",
+  appId: "1:373608190954:web:f32040073c79abbfe09893",
+  measurementId: "G-YCEP86N7K7"
+};
 
 @NgModule({
   declarations: [
@@ -33,8 +33,8 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
-    // FirestoreModule,
-    // AngularFireModule.initializeApp(firebaseConfig),
+    FirestoreModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- superstack/src/app/contact-form/contact-form.component.ts
+++ superstack/src/app/contact-form/contact-form.component.ts
@@ -1,3 +1,226 @@
+
+// import { Component, OnInit } from '@angular/core';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// // import { AngularFirestore } from '@angular/fire/compat/firestore';
+// import emailjs from 'emailjs-com';
+
+// @Component({
+//   selector: 'app-contact-form',
+//   templateUrl: './contact-form.component.html',
+//   styleUrls: ['./contact-form.component.css']
+// })
+// export class ContactFormComponent implements OnInit {
+//   title = "Contact Form";
+//   ContactForm!: FormGroup;
+//   isSubmit = false;
+//   submitMessage = "";
+
+//   // constructor(private formBuilder: FormBuilder, private firestore: AngularFirestore) { }
+//   constructor(private formBuilder: FormBuilder) { }
+
+//   ngOnInit() {
+//     this.ContactForm = this.formBuilder.group({
+//       name: ['', Validators.required],
+//       email: ['', [Validators.required, Validators.email]],
+//       msg: ['', Validators.required]
+//     });
+//   }
+
+//   onSubmit() {
+//     if (this.ContactForm.valid) {
+//       const formData = this.ContactForm.value;
+
+//       // Configure emailjs
+//       emailjs.init('YOUR_USER_ID');
+
+//       // Create email parameters
+//       const emailParams = {
+//         from_name: `${formData.name}`,
+//         to_name: 'Rohitash Singh',
+//         name: `${formData.name}`,
+//         email: `${formData.email}`,
+//         msg: `${formData.msg}`
+//       };
+
+//       // Send email
+//       emailjs.send('service_j4nsfvg', 'template_o5qkaf4', emailParams, 'Oatx7iGncadHGWeH_')
+//         .then((response) => {
+//           // Email sent successfully
+//           // console.log('Email sent:', response);
+
+//           // Save form data to Firestore
+//         //   this.firestore.collection('contacts').add(formData)
+//         //     .then(() => {
+//         //       this.submitMessage = "Message Sent Successfully";
+//         //       this.ContactForm.reset();
+//         //     })
+//         //     .catch(error => {
+//         //       console.error(error);
+//         //       this.submitMessage = "Message not sent!!";
+//         //     })
+//         //     .finally(() => {
+//         //       this.isSubmit = true;
+//         //       setTimeout(() => {
+//         //         this.isSubmit = false;
+//         //       }, 5000);
+//         //     });
+//         })
+//         .catch((error) => {
+//           // Email sending failed
+//           console.error('Email sending failed:', error);
+//         });
+//     }
+//   }
+// }
+
+
+
+// *************************** send data in firbase and send email both ************************** //
+
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { AngularFirestore } from '@angular/fire/compat/firestore';
+// import emailjs from 'emailjs-com';
+
+// @Component({
+//   selector: 'app-contact-form',
+//   templateUrl: './contact-form.component.html',
+//   styleUrls: ['./contact-form.component.css']
+// })
+// export class ContactFormComponent implements OnInit {
+//   title = "Contact Form";
+//   ContactForm!: FormGroup;
+//   isSubmit = false;
+//   submitMessage = "";
+
+//   constructor(private formBuilder: FormBuilder, private firestore: AngularFirestore) { }
+
+//   ngOnInit() {
+//     this.ContactForm = this.formBuilder.group({
+//       name: ['', Validators.required],
+//       email: ['', [Validators.required, Validators.email]],
+//       msg: ['', Validators.required]
+//     });
+//   }
+
+//   onSubmit() {
+//     if (this.ContactForm.valid) {
+//       const formData = this.ContactForm.value;
+
+//       // Configure emailjs
+//       emailjs.init('YOUR_USER_ID');
+
+//       // Create email parameters
+//       const emailParams = {
+//         from_name: `${formData.name}`,
+//         to_name: 'Rohitash Singh',
+//         name: `${formData.name}`,
+//         email: `${formData.email}`,
+//         msg: `${formData.msg}`
+//       };
+
+//       // Send email
+//       emailjs.send('service_j4nsfvg', 'template_o5qkaf4', emailParams, 'Oatx7iGncadHGWeH_')
+//         .then((response) => {
+//           // Email sent successfully
+//           // console.log('Email sent:', response);
+
+//           // Save form data to Firestore
+//           this.firestore.collection('contacts').add(formData)
+//             .then(() => {
+//               this.submitMessage = "Message Sent Successfully";
+//               this.ContactForm.reset();
+//             })
+//             .catch(error => {
+//               console.error(error);
+//               this.submitMessage = "Message not sent!!";
+//             })
+//             .finally(() => {
+//               this.isSubmit = true;
+//               setTimeout(() => {
+//                 this.isSubmit = false;
+//               }, 5000);
+//             });
+//         })
+//         .catch((error) => {
+//           // Email sending failed
+//           console.error('Email sending failed:', error);
+//         });
+//     }
+//   }
+// }
+
+
+
+
+
+// ********************************** send data to contact api ********************************* //
+
+// import { Component, OnInit } from '@angular/core';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+// @Component({
+//   selector: 'app-contact-form',
+//   templateUrl: './contact-form.component.html',
+//   styleUrls: ['./contact-form.component.css']
+// })
+// export class ContactFormComponent implements OnInit {
+//   title = "Contact Form";
+//   ContactForm!: FormGroup;
+//   isSubmit = false;
+//   submitMessage = "";
+
+//   constructor(private formBuilder: FormBuilder, private firestore: AngularFirestore) { }
+
+//   ngOnInit() {
+//     this.ContactForm = this.formBuilder.group({
+//       name: ['', Validators.required],
+//       email: ['', [Validators.required, Validators.email]],
+//       msg: ['', Validators.required]
+//     });
+//   }
+
+//   onSubmit() {
+//     if (this.ContactForm.valid) {
+//       const formData = this.ContactForm.value;
+
+//       this.firestore.collection('contacts').add(formData)
+//         .then(() => {
+//           this.submitMessage = "Message Sent Successfully";
+//           this.ContactForm.reset();
+//         })
+//         .catch(error => {
+//           console.error(error);
+//           this.submitMessage = "Message not sent!!";
+//         })
+//         .finally(() => {
+//           this.isSubmit = true;
+//           setTimeout(() => {
+//             this.isSubmit = false;
+//           }, 5000);
+//         });
+//     }
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import axios from 'axios';
@@ -11,7 +234,7 @@ import axios from 'axios';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
-  title="Contact Form";
+  title = "Contact Form";
 
   ContactForm!: FormGroup;
   isSubmit = true;
@@ -19,7 +242,7 @@ export class ContactFormComponent implements OnInit {
 
   // contactCollection!: AngularFirestoreCollection<any>;
   // constructor(private formBuilder: FormBuilder, private afs: AngularFirestore) { 
-  constructor(private formBuilder: FormBuilder) { 
+  constructor(private formBuilder: FormBuilder) {
 
   }
 
@@ -29,15 +252,24 @@ export class ContactFormComponent implements OnInit {
     this.ContactForm = this.formBuilder.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      msg: ['', Validators.required]
+      msg: ['', Validators.required],
+      phone: ['', Validators.required],
+      gst: ['', Validators.required],
+      pan: ['', Validators.required],
+      kyc_update: [''],
+      prod_details: [''],
+      state: [''],
+      details: ['']
     });
   }
 
   onSubmit() {
     if (this.ContactForm.valid) {
       const formData = this.ContactForm.value;
-      console.log(formData);
-  
+      console.log(formData.kyc_update);
+      console.log(formData.details)
+      console.log(formData.prod_details)
+
       axios
         .post('http://localhost:8000/contact', formData)
         .then(response => {
@@ -57,5 +289,5 @@ export class ContactFormComponent implements OnInit {
         });
     }
   }
-  
+
 }
